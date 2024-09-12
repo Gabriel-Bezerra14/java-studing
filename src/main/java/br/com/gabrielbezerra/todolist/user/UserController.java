687e4acdf493b3a2f04d5817f26affbe646915c6
Gabriel-Bezerra14 <gabrielbezerradev@gmail.com>
2024-09-12 11:48:57 -0300

    importação de users e tasks no banco de dados.

--- src/main/java/br/com/gabrielbezerra/todolist/user/UserController.java
+++ src/main/java/br/com/gabrielbezerra/todolist/user/UserController.java
@@ -1,6 +1,10 @@
 package br.com.gabrielbezerra.todolist.user;
 
 
+import at.favre.lib.crypto.bcrypt.BCrypt;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,8 +13,26 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/users")
 public class UserController {
+
+    @Autowired
+    private IUserRepository userRepository;
+
     @PostMapping("/")
-    public void create(@RequestBody UserModel userModel) {
-        System.out.println(userModel.getUsername());
+    public ResponseEntity create(@RequestBody UserModel userModel) {
+        var user = this.userRepository.findByUsername(userModel.getUsername());
+
+        if(user != null) {
+            System.out.println("Usuário já existe");
+            //Mensagem de erro
+            //status code
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Usuário já existe");
+        }
+
+        var passwordHasheed = BCrypt.withDefaults().hashToString(12, userModel.getPassword().toCharArray());
+
+        userModel.setPassword(passwordHasheed);
+
+        var userCreated = this.userRepository.save(userModel);
+        return ResponseEntity.status(HttpStatus.CREATED).body(userCreated);
     }
 }
