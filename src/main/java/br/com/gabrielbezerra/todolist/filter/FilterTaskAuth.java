c1243f309a5e0e2a799a90ae765defdd8179efae
Gabriel-Bezerra14 <gabrielbezerradev@gmail.com>
2024-09-17 16:21:48 -0300

    Pegar a autenticação (usuario e senha)

--- src/main/java/br/com/gabrielbezerra/todolist/filter/FilterTaskAuth.java
+++ src/main/java/br/com/gabrielbezerra/todolist/filter/FilterTaskAuth.java
@@ -1,12 +1,11 @@
 package br.com.gabrielbezerra.todolist.filter;
 
-
 import jakarta.servlet.*;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.apache.tomcat.util.http.parser.Authorization;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
-
 import java.io.IOException;
 import java.util.Base64;
 
@@ -16,20 +15,33 @@ public class FilterTaskAuth extends OncePerRequestFilter {
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
 
-        //Pegar a autenticação (usuario e password)
-        var autorization = request.getHeader("Autorization");
-        var authEncondded = autorization.substring("Basic ".length()).trim();
-        Base64.getDecoder().decode(authEncondded);
+        //Pegar a autenticação (usuario e senha)
 
-        System.out.println("Autorization");
-        System.out.println(autorization);
+        var authorization = request.getHeader("Authorization");
+
+        var authEncoded = authorization.substring("Basic".length()).trim();
+
+        byte[] authDecode = Base64.getDecoder().decode(authEncoded);
+
+        var authString = new String(authDecode);
 
 
-        // Validar usuario
+        //["ligeirinho14x", "12345"]
+        String[] credentials = authString.split(":");
+        String username = credentials[0];
+        String password = credentials[1];
 
-        // Validar Senha
+        System.out.println("Authorization");
+        System.out.println(username);
+        System.out.println(password);
 
-        // Segue viagem
+
+
+        //Validar usuario
+
+        //Validar a senha
+
+        //Segue viagem
         filterChain.doFilter(request, response);
     }
 }
