0e02e0e96a0929d79b091866a2edc4df7a948e0d
Gabriel-Bezerra14 <gabrielbezerradev@gmail.com>
2024-09-23 17:07:20 -0300

    Validações de usuários

--- src/main/java/br/com/gabrielbezerra/todolist/filter/FilterTaskAuth.java
+++ src/main/java/br/com/gabrielbezerra/todolist/filter/FilterTaskAuth.java
@@ -45,7 +45,7 @@ public class FilterTaskAuth extends OncePerRequestFilter {
                     //Validar a senha
                 var passwordVerify = BCrypt.verifyer().verify(password.toCharArray(), user.getPassword());
                 if (passwordVerify.verified) {
-                    request.setAttribute("idUser", user.getId());
+                    request.setAttribute("id", user.getId());
                     filterChain.doFilter(request, response);
                 } else {
                     response.sendError(401);
